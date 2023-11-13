814cd1aeefe700a4331d72098f718deef9c73dad
준혁 <127603139+HYH0804@users.noreply.github.com>
2023-11-13 17:08:57 +0900

    Title: 1-9 개수 세기, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 231113/1-9 개수 세기/count-one-to-nine.py
+++ 231113/1-9 개수 세기/count-one-to-nine.py
@@ -1,10 +1,11 @@
 n= int(input())
 
 arr=list(map(int,input().split()))
-cnt=0
-for i in range(1,10):
-    for j in range(n):
-        if arr[j]==i:
-            cnt+=1
-    print(cnt)
-    cnt=0
+
+ar = [0]*9
+
+for i in range(len(arr)):
+    ar[arr[i]-1]+=1
+
+for j in range(9):
+    print(ar[j])

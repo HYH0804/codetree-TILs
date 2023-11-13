6c06a77ae21b151678457068e297345de98f0e28
준혁 <127603139+HYH0804@users.noreply.github.com>
2023-11-13 13:56:28 +0900

    Title: 짝수인 것만 출력하기, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231113/짝수인 것만 출력하기/print-even-number.py
@@ -0,0 +1,9 @@
+n=int(input())
+arr =list(map(int,input().split()))
+ar= list()
+for i in range(n):
+    if arr[i] %2 == 0:
+        ar.append(arr[i])
+
+for j in range(len(ar)):
+    print(ar[j],end=" ")

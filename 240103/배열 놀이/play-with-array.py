2cd98ef78a04d67b178cc652463412be5478a36f
준혁 <127603139+HYH0804@users.noreply.github.com>
2024-01-03 13:55:29 +0900

    Title: 배열 놀이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240103/배열 놀이/play-with-array.py
@@ -0,0 +1,18 @@
+ele , qe = list(map(int,input().split()))
+ele_list = list(map(int,input().split()))
+for _ in  range(qe) :
+    q_list = list(map(int,input().split()))
+    if q_list[0] == 1:
+        print(ele_list[q_list[1]-1])
+    elif q_list[0]==2:
+        a =ele_list.index(q_list[1])
+        if  a!= -1:
+            print(a+1)
+        else:
+            print(0)
+    else : 
+        cnt = q_list[1]
+        cnt_end = q_list[2]
+        while cnt<=cnt_end:
+            print(ele_list[cnt-1],end=" ")
+            cnt+=1

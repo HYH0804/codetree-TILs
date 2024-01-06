e12f2cc43894c35bb02875d929536480d19b30a4
준혁 <127603139+HYH0804@users.noreply.github.com>
2024-01-06 15:59:45 +0900

    Title: 배열의 평균, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240106/배열의 평균/ave-of-array.py
+++ 240106/배열의 평균/ave-of-array.py
@@ -3,7 +3,7 @@ total=0
 arr_2 = list()
 for i in range(2):
     arr_1=list(map(int,input().split()))
-    total = sum(arr_1)
+    total += sum(arr_1)
     avg = sum(arr_1)/4.0
     print(avg, end=" ")
     arr_2.append(arr_1)
@@ -17,4 +17,4 @@ for i in range(4):
     sm=0
 print()
 
-print(total_avg/8.0)
+print(total/8.0)

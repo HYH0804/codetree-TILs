b58500479dfd36eeeacd940357110448679b2922
준혁 <127603139+HYH0804@users.noreply.github.com>
2024-02-28 18:13:21 +0900

    Title: 알파벳 지우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240228/알파벳 지우기/remove-alphabet.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+        String str2 = sc.next();
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+        char[] t1 = new char[10];
+        char[] t2 = new char[10];
+        int cnt1=0;
+        int cnt2=0;
+        for(int i =0; i<arr1.length; i++){
+            if('0'<=arr1[i] && arr1[i]<='9'){
+                t1[cnt1]=arr1[i];
+                cnt1++;
+            }
+            if('0'<=arr2[i] && arr2[i]<='9'){
+                t2[cnt2]=arr2[i];
+                cnt2++;
+            }
+        }
+        String a = "";
+        String b = "";
+        for(int i =0; i<cnt1; i++){
+            a=a+t1[i];
+            }
+        for(int i =0; i<cnt2; i++){
+            b=b+t2[i];
+            }
+        System.out.printf("%d",Integer.parseInt(a)+Integer.parseInt(b));
+    }
+}

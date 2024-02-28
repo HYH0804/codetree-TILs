6a681513dcb8119de1392601650ac1480dfcebad
준혁 <127603139+HYH0804@users.noreply.github.com>
2024-02-28 15:37:47 +0900

    Title: a로 채워넣기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240228/a로 채워넣기/filling-with-a.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        char[] arr = sc.next().toCharArray();
+        arr[1] = 'a';
+        arr[arr.length-2] = 'a';
+        for(int i =0; i<arr.length; i++){
+            System.out.printf("%c",arr[i]);
+        }
+    }
+}

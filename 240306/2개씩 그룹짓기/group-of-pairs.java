26870c6d7ba51e0fac5ce6e06f5696ad0ecdb98e
준혁 <127603139+HYH0804@users.noreply.github.com>
2024-03-06 10:40:55 +0900

    Title: 2개씩 그룹짓기, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240306/2개씩 그룹짓기/group-of-pairs.java
+++ 240306/2개씩 그룹짓기/group-of-pairs.java
@@ -11,7 +11,7 @@ public class Main {
             arr[i]=sc.nextInt();
         }
         Arrays.sort(arr);
-        if(N==1){
+        if(N!=1){
             for(int i=1; i<N; i++){
                 int temp= arr[i]+arr[2*N-1-i];
                 max=Math.max(temp,max);

822e08ab0e4969d0fea20ff279ac9d430c88aca4
준혁 <127603139+HYH0804@users.noreply.github.com>
2024-03-25 10:08:47 +0900

    Title: 블럭쌓는 명령2, Time: 179ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240325/블럭쌓는 명령2/block-stacking-commands2.java
@@ -0,0 +1,35 @@
+import java.util.*;
+//? 뭐야 시발 
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc= new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int[] arr = new int[N];
+        for(int i=0; i<N; i++){
+            arr[i]=0;
+        }
+        for(int i =0; i<K; i++){
+            int start = sc.nextInt()-1;
+            int end = sc.nextInt()-1;
+            if(start!=end){
+                for(int j=start; j<=end; j++){
+                    arr[j]++;
+                }
+            }
+            else{
+                arr[start]++;
+            }
+        }
+        int maxIndex =0;
+        for(int i =1; i<N; i++){
+            if(arr[maxIndex]<arr[i]){
+                maxIndex=i;
+            }
+        }
+        System.out.print(arr[maxIndex]);
+
+
+    }
+}

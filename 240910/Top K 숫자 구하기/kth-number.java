8a5ec9aaa81c50915184f44213b2d394721ce22c
준혁 <127603139+HYH0804@users.noreply.github.com>
2024-09-10 15:56:32 +0900

    Title: Top K 숫자 구하기, Time: 197ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240910/Top K 숫자 구하기/kth-number.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr= new int[n];
+        for(int i=0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[k-1]);
+    }
+}
